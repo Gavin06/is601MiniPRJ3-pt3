59bb32012748efc7e7bf1dd46dc74f6738da08e2
gdp9 <gdp9@njit.edu>
2019-05-16 22:41:58 -0400

    Feature#7. Added a function of updating an existing record for answer

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -28,8 +28,8 @@
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header"><a class="btn btn-primary float-left"
-                                                href="#">
-                            Answer Question
+                                                href="{{ route('answers.create', ['question_id'=> $question->id])}}">
+                            Reply
                         </a></div>
 
                     <div class="card-body">
@@ -39,7 +39,7 @@
                                 <div class="card-footer">
 
                                     <a class="btn btn-primary float-right"
-                                       href="{{ route('answer.show', ['question_id'=> $question->id,'answer_id' => $answer->id]) }}">
+                                       href="{{ route('answers.show', ['question_id'=> $question->id,'answer_id' => $answer->id]) }}">
                                         View
                                     </a>
 
