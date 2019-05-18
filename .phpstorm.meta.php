ae2b963a54df3ebaeaeb82165a23ab785fa86b55
gdp9 <gdp9@njit.edu>
2019-05-18 17:11:48 -0400

    added lock file

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-05-18 17:13:46.
+    * Generated on 2019-05-18 20:09:26.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
